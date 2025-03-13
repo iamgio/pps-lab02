6f8ad8a5b25919720d6edfee343ccd3882ac5359
Giorgio Garofalo <giogar2705@gmail.com>
2025-03-13 22:08:44 +0100

    Add reverse function

--- src/test/scala/lab/RecursionTest.scala
+++ src/test/scala/lab/RecursionTest.scala
@@ -10,3 +10,7 @@ class RecursionTest:
   @Test def testPower(): Unit =
     assertEquals(9, power(3.0, 2).toInt)
     assertEquals(125, power(5.0, 3).toInt)
+
+  @Test def testReverse(): Unit =
+    assertEquals(54321, reverseNumber(12345))
+    assertEquals(72901, reverseNumber(10927))

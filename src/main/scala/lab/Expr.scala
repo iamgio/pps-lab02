c040a11b01d3acb49b8692e944061a546aead6b0
Giorgio Garofalo <giogar2705@gmail.com>
2025-03-13 22:40:31 +0100

    Add Expr functions

---
+++ src/main/scala/lab/Expr.scala
@@ -0,0 +1,32 @@
+package lab
+
+import lab.Expr.{Add, Literal, Multiply}
+
+enum Expr:
+  case Literal(value: Int)
+  case Add(left: Expr, right: Expr)
+  case Multiply(left: Expr, right: Expr)
+
+def evaluate(expr: Expr): Int =
+  expr match
+    case Literal(value) => value
+    case Add(left, right) => evaluate(left) + evaluate(right)
+    case Multiply(left, right) => evaluate(left) * evaluate(right)
+
+def show(expr: Expr): String =
+  def join(left: String, right: String, operator: String, wrap: Boolean) =
+    left + operator + right match
+      case result if wrap => s"($result)"
+      case result if !wrap => result
+
+
+  def _show(expr: Expr, root: Boolean): String =
+    def joinExpressions(left: Expr, right: Expr)(operator: String) =
+      join(_show(left, false), _show(right, false), operator, !root)
+
+    expr match
+      case Literal(value) => value.toString
+      case Add(left, right) => joinExpressions(left, right)("+")
+      case Multiply(left, right) => joinExpressions(left, right)("*")
+
+  _show(expr, true)
---
+++ src/test/scala/lab/ExprTest.scala
@@ -0,0 +1,41 @@
+package lab
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ *
+ */
+class ExprTest:
+  @Test def testEvaluate(): Unit = {
+    assertEquals(5, evaluate(Expr.Literal(5)))
+    assertEquals(12, evaluate(Expr.Add(Expr.Literal(5), Expr.Literal(7))))
+    assertEquals(15, evaluate(Expr.Multiply(Expr.Literal(5), Expr.Literal(3))))
+    assertEquals(24, evaluate(
+      Expr.Multiply(
+        Expr.Literal(2),
+        Expr.Add(Expr.Literal(5), Expr.Literal(7))
+      )
+    ))
+  }
+
+  @Test def testShow(): Unit = {
+    assertEquals("5", show(Expr.Literal(5)))
+    assertEquals("5+7", show(Expr.Add(Expr.Literal(5), Expr.Literal(7))))
+    assertEquals("5*3", show(Expr.Multiply(Expr.Literal(5), Expr.Literal(3))))
+    assertEquals("2*(5+7)", show(
+      Expr.Multiply(
+        Expr.Literal(2),
+        Expr.Add(Expr.Literal(5), Expr.Literal(7))
+      )
+    ))
+    assertEquals("2*((5+7)+4)", show(
+      Expr.Multiply(
+        Expr.Literal(2),
+        Expr.Add(
+          Expr.Add(Expr.Literal(5), Expr.Literal(7)),
+          Expr.Literal(4)
+        )
+      )
+    ))
+  }

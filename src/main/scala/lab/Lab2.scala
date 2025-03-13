86888880157f98ba232161646d5b1531a183bddf
Giorgio Garofalo <giogar2705@gmail.com>
2025-03-13 23:13:11 +0100

    Adjust task names

--- src/main/scala/lab/Lab2.scala
+++ src/main/scala/lab/Lab2.scala
@@ -3,8 +3,8 @@ package lab
 import scala.annotation.tailrec
 
 object Lab2 extends App:
-  // Task 3, svolto da solo
-  
+  // Task 2, svolto da solo
+
   val positiveLiteral = (x: Int) => x match
     case n if n >= 0 => "positive"
     case _ => "negative"
@@ -12,14 +12,12 @@ object Lab2 extends App:
   def positiveMethod(x: Int) = x match
     case n if n >= 0 => "positive"
     case _ => "negative"
-  
+
   val negLiteral: (String => Boolean) => String => Boolean = predicate => !predicate(_)
 
   def negMethod(predicate: String => Boolean): String => Boolean = !predicate(_)
-  
-  def negGeneric[X](predicate: X => Boolean): X => Boolean = !predicate(_)
 
-  // Task 4, svolto da solo
+  def negGeneric[X](predicate: X => Boolean): X => Boolean = !predicate(_)
 
   val nonCurriedPredicateLiteral: (Int, Int, Boolean) => Boolean = _ <= _ == _
 
@@ -30,21 +28,17 @@ object Lab2 extends App:
 
   def curriedPredicateMethod(x: Int, y: Int)(z: Boolean) = x <= y == z
 
-  // Task 5, svolto da solo
-
   def compose(f: Int => Int, g: Int => Int): Int => Int =
     x => f(g(x))
 
   def composeGeneric[A, B, C](f: B => C, g: A => B): A => C =
     x => f(g(x))
-    
-  // Task 6, svolto da solo
 
   def composeThree[A, B, C, D](f: C => D, g: B => C, h: A => B): A => D =
     composeGeneric(f, x => g(h(x)))
 
-  // Task 7, svolto da solo
-    
+  // Task 3, svolto da solo
+
   def power(base: Double, exponent: Int): Double =
     @tailrec
     def _power(base: Double, exponent: Int, accumulator: Double): Double =
@@ -55,8 +49,6 @@ object Lab2 extends App:
 
     _power(base, exponent, 1.0)
 
-  // Task 8, svolto da solo
-  
   def reverseNumber(n: Int): Int =
     @tailrec
     def _reverse(n: Int, accumulator: Int): Int =
@@ -66,7 +58,7 @@ object Lab2 extends App:
 
     _reverse(n, 0)
 
-  // Task 9, svolto da solo
+  // Task 4, svolto da solo
 
   enum Expr:
     case Literal(value: Int)
